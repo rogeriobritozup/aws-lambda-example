26ae57c4250650a06fc429f1f34b969daeb51963
Rogério Brito <rogerio.brito@zup.com.br>
2021-11-22 18:25:49 -0300

    improvements in lambda code

--- lambda-code/lambda_function.py
+++ lambda-code/lambda_function.py
@@ -1,12 +1,19 @@
 import requests
 import json
 
+def proccess_sqs_record(record):
+    sqs_body = json.loads(record["body"])
+    url = sqs_body["url"]
+    print("requesting url {}".format(url))
+    r = requests.get(url)
+    print("response code: {}".format(r.status_code))
+    print("response body: {}".format(r.json()))
+
 def handler(event, context):
     records = event["Records"]
+    
     for record in records:
-        sqs_body = json.loads(record["body"])
-        url = sqs_body["url"]
-        print("requesting url {}".format(url))
-        r = requests.get(url)
-        print("response code: {}".format(r.status_code))
-        print("response body: {}".format(r.json()))
+        try:
+            proccess_sqs_record(record)
+        except Exception as e:
+            print("error on procces sqs record: {}".format(e))
